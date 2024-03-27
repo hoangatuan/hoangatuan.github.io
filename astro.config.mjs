6bfaf9c761eeef6e1061257d9bb670174be19561
tuanhoang <tuanhanh19981998@gmail.com>
2024-03-27 22:25:56 +0800

    Update Header & Create footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,26 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    icon({
+      include: {
+        tabler: ['*'],
+        'flat-color-icons': [
+          'template',
+          'gallery',
+          'approval',
+          'document',
+          'advertising',
+          'currency-exchange',
+          'voice-presentation',
+          'business-contact',
+          'database',
+        ],
+      },
+    }),
+  ]
 });
---
+++ docs/css.md
@@ -0,0 +1,13 @@
+
+## 1. Margin
+
+- used to create space around elements, outside of any defined borders.
+- auto: to horizontally center the element within its container.
+
+## 2. Padding
+
+- used to create space around an element's content, inside of any defined borders.
+
+## 3. Height/width
+
+- max-width max-height
