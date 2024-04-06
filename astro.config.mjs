750e9bfaf23c1bba71e7f7bff5a43ac47d7cfadd
tuanhoang <tuanhanh19981998@gmail.com>
2024-04-06 18:06:37 +0800

    Add search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,25 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import starlight from '@astrojs/starlight';
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    starlight({
+      title: 'Site with DocSearch',
+      // plugins: [
+      //   starlightDocSearch({
+      //     appId: 'MAM15PO3N4',
+      //     apiKey: 'a5d4344c07f11031a5943c14e9b2ebc7',
+      //     indexName: 'posts',
+      //   }),
+      // ],
+      components: {
+        // Override the default `Search` component.
+        Search: './src/components/search/Search.astro'
+      }
+    }),
     tailwind(),
     icon({
       include: {
