065bacf38e9715c8c0b8202aad852991800d4d01
tuanhoang <tuanhanh19981998@gmail.com>
2024-12-09 23:12:05 +0800

    Change to use Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sectionize from '@hbsnow/rehype-sectionize';
 
 // https://www.freecodecamp.org/news/how-to-add-google-analytics-to-your-astro-website/
 import partytown from '@astrojs/partytown'
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
@@ -31,4 +32,5 @@ export default defineConfig({
     remarkPlugins: [readingTimeRemarkPlugin],
     rehypePlugins: [responsiveTablesRehypePlugin, sectionize],
   },
+  adapter: netlify(),
 });
